4df143b381b5c07669f72361b6330b0d73e1c925
qtipee <quent.vallat@gmail.com>
2020-04-13 19:08:46 +0200

    Create local_settings.py

---
+++ safm_project/safm_project/settings/local_settings.py
@@ -0,0 +1,7 @@
+from safm_project.settings.common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
