f93962fe2db8580e6c9df6f0f30dff5fead2da89
qtipee <quent.vallat@gmail.com>
2020-04-13 20:49:55 +0200

    Removed local_settings.py

--- safm_project/safm_project/settings/local_settings.py
+++
@@ -1,7 +0,0 @@
-from safm_project.settings.common import *
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
