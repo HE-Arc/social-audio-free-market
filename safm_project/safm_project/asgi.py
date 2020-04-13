8d2e514c34900d199df47abc22090e7e573a2826
qtipee <quent.vallat@gmail.com>
2020-04-13 15:33:23 +0200

    Setup configuration

--- safm_project/safm_project/asgi.py
+++ safm_project/safm_project/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'safm_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'safm_project.settings.development')
 
 application = get_asgi_application()
---
+++ safm_project/safm_project/settings/production.py
@@ -0,0 +1,9 @@
+from safm_project.settings.common import *
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# SECURITY WARNING: update this when you have the production host
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
