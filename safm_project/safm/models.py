0d0e7bb07b028fe5fea40eb044eac4b6688879dc
qtipee <quent.vallat@gmail.com>
2020-04-14 23:15:03 +0200

    Sample model

--- safm_project/safm/models.py
+++ safm_project/safm/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Sample(models.Model):
+
+    class Tone(models.TextChoices):
+        A = 'A'
+        B = 'B'
+        C = 'C'
+        D = 'D'
+        E = 'E'
+        F = 'F'
+        G = 'G'
+
+    class Mode(models.TextChoices):
+        MINOR = 'm', 'Minor'
+        MAJOR = 'M', 'Major'
+
+    def user_directory_path(instance, filename):
+        # File will be uploaded to MEDIA_ROOT/samples/<username>/<filename>
+        return 'samples/{0}/{1}'.format(instance.user.username, filename)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    file = models.FileField(max_length=255, upload_to=user_directory_path)
+    duration = models.FloatField(blank=True, null=True) # duration in [s]
+    tempo = models.PositiveIntegerField(blank=True, null=True) # tempo is > 0
+    tone = models.CharField(max_length=1, choices=Tone.choices, blank=True)
+    mode = models.CharField(max_length=1, choices=Mode.choices, blank=True)
+    datetime_upload = models.DateTimeField(auto_now_add=True) # auto now at creation
+    nb_dl_unauthenticated = models.PositiveIntegerField(default=0) # nb dl > 0
