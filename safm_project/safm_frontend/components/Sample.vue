c92d4f8a21973db5991bfff26142a3d64a53631c
qtipee <quent.vallat@gmail.com>
2020-04-15 16:23:09 +0200

    Init Sample Vue component

---
+++ safm_project/safm_frontend/components/Sample.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sample">
+    <h3>{{ name }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: 'Sample#1'
+    }
+  }
+}
+</script>
