175ce86e48667e211cc9fbce5b1e629f030a721d
qtipee <quent.vallat@gmail.com>
2020-04-15 14:25:09 +0200

    Fixed lint errors in Nuxt.js test example

--- safm_project/safm_frontend/test/index.test.js
+++ safm_project/safm_frontend/test/index.test.js
@@ -1,7 +1,7 @@
-let test = require('ava')
+const test = require('ava')
 
 // Example Test
-test('Example Test', async (t) => {
+test('Example Test', (t) => {
   const html = '<h1>Title</h1>'
   t.true(html.includes('Title'))
 })
