f169d62c199eaab503d82ed369ae85180b6cb944
qtipee <quent.vallat@gmail.com>
2020-04-26 12:45:16 +0200

    Adds Sample page + Comments component (very basic)

---
+++ safm_project/safm_frontend/components/Comments.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="comments">
+        <h2>Comments</h2>
+        <div
+            class="comment"
+            v-for="comment in comments"
+            :key="comment.id"
+        >
+            <p>{{comment.username }}</p>
+            <p>{{ comment.text }}</p>
+            <p>{{ comment.datetime }}</p>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    props: ['comments']
+}
+</script>
---
+++ safm_project/safm_frontend/pages/samples/_id/index.vue
@@ -0,0 +1,54 @@
+<template>
+    <main>
+        <v-container>
+            <Sample :sample="sample" />
+
+            <Comments :comments="comments" />
+
+        </v-container>
+    </main>
+</template>
+
+<script>
+import Sample from '~/components/Sample.vue'
+import Comments from '~/components/Comments.vue'
+
+export default {
+    components: {
+        Sample,
+        Comments
+    },
+
+    data () {
+        return {
+            sample: [],
+            comments: []
+        }
+    },
+
+    async asyncData({ $axios, params }) {
+        try {
+            let sample = await $axios.$get(`/sample/${params.id}`)
+
+            // THIS IS TEMPORARY ; WILL BE DEVELOPED LATER
+            let comments = [
+                {
+                    id: 1,
+                    username: 'Len Faki',
+                    text: 'Aliquip laborum dolor ex ut ut qui ipsum. Ullamco tempor deserunt incididunt veniam. Pariatur veniam ad tempor sit. Ipsum dolore pariatur aliqua veniam eu est.',
+                    datetime: '26.04.2020 14:30'
+                },
+                {
+                    id: 2,
+                    username: 'Solomun',
+                    text: 'Amet dolor laboris ea ad mollit elit nulla aliquip. Occaecat consectetur commodo culpa excepteur consectetur occaecat. Ullamco culpa occaecat cillum ipsum deserunt culpa reprehenderit. Occaecat pariatur et reprehenderit eu labore nisi non consequat ipsum nisi tempor ullamco fugiat Lorem. Ut Lorem qui id eu velit id ut fugiat sit cupidatat. Sunt voluptate ex voluptate cillum proident magna.',
+                    datetime: '26.04.2020 14:43'
+                }
+            ]
+            return { sample, comments }
+        } catch (e) {
+            return { sample: [], comments: [] }
+        }
+    }
+}
+</script>
