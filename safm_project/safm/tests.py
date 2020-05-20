75ed240a97ddfd7ab07524f796f7dcfec812f8c5
qtipee <quent.vallat@gmail.com>
2020-05-20 14:16:40 +0200

    Unit testing for Advanced Search

--- safm_project/safm/tests.py
+++ safm_project/safm/tests.py
@@ -1,4 +1,5 @@
 import os
+import re
 import json
 from rest_framework import status
 from django.test import TestCase
@@ -69,7 +70,7 @@ class SampleTest(TestCase):
                 user=user,
                 name=filename,
                 file=file,
-                tone=Sample.Tone.A,
+                key=Sample.Key.A,
                 mode=Sample.Mode.MAJOR
             )
             sample.save()
@@ -193,7 +194,7 @@ class QuickSearchTest(TestCase):
                 if search_query in str(sample['fields']['tempo']):
                     count += 1
                     continue
-                if search_query in sample['fields']['tone']:
+                if search_query in sample['fields']['key']:
                     count += 1
                     continue
                 if search_query in sample['fields']['mode']:
@@ -225,3 +226,155 @@ class QuickSearchTest(TestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(jsonResponse), 0)
+
+
+class AdvancedSearchTest(TestCase):
+    '''
+    Advanced Search unit testing
+    '''
+    fixtures = ['users.json', 'samples.json', 'tags.json']
+
+    def setUp(self):
+        self.client = Client()
+
+        # Reads the users fixture file
+        with open('./safm/fixtures/users.json') as json_users:
+            self.users = json.load(json_users)
+
+        # Reads the samples fixture file
+        with open('./safm/fixtures/samples.json') as json_samples:
+            self.samples = json.load(json_samples)
+
+        # Reads the tags fixture file
+        with open('./safm/fixtures/tags.json') as json_tags:
+            self.tags = json.load(json_tags)
+
+    @tag('advanced_search_name')
+    def test_advanced_search_name(self):
+        '''
+        Checks that the Advanced Search returns the correct number
+        of samples based on the name property.
+        '''
+        for n in ['', 'techno', 'acid', 'kick', 'vocal', 'bass']:
+            count = 0
+            for sample in self.samples:
+                name = sample['fields']['name']
+                if re.search(n, name, re.IGNORECASE):
+                    count += 1
+            
+            url = '/api/ad_search?name__icontains={0}'.format(n)
+            response = self.client.get(url)
+            jsonResponse = json.loads(response.content)
+
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(len(jsonResponse), count)
+
+    @tag('advanced_search_username')
+    def test_advanced_search_username(self):
+        '''
+        Checks that the Advanced Search returns the correct number
+        of samples based on the user__username property.
+        '''
+        for user in self.users:
+            username = user['fields']['username']
+            count = 0
+            for sample in self.samples:
+                user__username = User.objects.get(pk=sample['fields']['user']).username
+                if user__username == username:
+                    count += 1
+
+            url = '/api/ad_search?user__username__icontains={0}'.format(username)
+            response = self.client.get(url)
+            jsonResponse = json.loads(response.content)
+
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(len(jsonResponse), count)
+
+    @tag('advanced_search_duration')
+    def test_advanced_search_duration(self):
+        '''
+        Checks that the Advanced Search returns the correct number
+        of samples based on the duration property.
+        '''
+        duration__gte = 3.0
+        duration__lte = 5.0
+
+        count = 0
+        for sample in self.samples:
+            duration = sample['fields']['duration']
+            if duration >= duration__gte and duration <= duration__lte:
+                count += 1
+        
+        url = '/api/ad_search?duration__lte={0}&duration__gte={1}'.format(duration__lte, duration__gte)
+        response = self.client.get(url)
+        jsonResponse = json.loads(response.content)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(jsonResponse), count)
+
+    @tag('advanced_search_tempo')
+    def test_advanced_search_tempo(self):
+        '''
+        Checks that the Advanced Search returns the correct number
+        of samples based on the tempo property.
+        '''
+        tempo__gte = 125
+        tempo__lte = 135
+
+        count = 0
+        for sample in self.samples:
+            tempo = sample['fields']['tempo']
+            if tempo >= tempo__gte and tempo <= tempo__lte:
+                count += 1
+        
+        url = '/api/ad_search?tempo__lte={0}&tempo__gte={1}'.format(tempo__lte, tempo__gte)
+        response = self.client.get(url)
+        jsonResponse = json.loads(response.content)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(jsonResponse), count)
+
+    @tag('advanced_search_key')
+    def test_advanced_search_key(self):
+        '''
+        Checks that the Advanced Search returns the correct number
+        of samples based on the key property.
+        '''
+        keys = [e.value for e in Sample.Key]
+        for k in keys:
+            count = 0
+            for sample in self.samples:
+                key = sample['fields']['key']
+                if key == k:
+                    count += 1
+
+            url = '/api/ad_search?key={0}'.format(k)
+            response = self.client.get(url)
+            jsonResponse = json.loads(response.content)
+
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(len(jsonResponse), count)
+
+    @tag('advanced_search_mode')
+    def test_advanced_search_mode(self):
+        '''
+        Checks that the Advanced Search returns the correct number
+        of samples based on the mode property.
+        '''
+        for m in ['', 'm', 'M']:
+            count = 0
+            for sample in self.samples:
+                mode = sample['fields']['mode']
+                if m == '' or m == mode:
+                    count += 1
+
+            url = '/api/ad_search?mode={0}'.format(m)
+            response = self.client.get(url)
+            jsonResponse = json.loads(response.content)
+
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(len(jsonResponse), count)
+        
+
+    # TODO: tags advanced search test ; depends on AND or OR condition
+    
