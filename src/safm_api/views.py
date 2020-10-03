6dc0a7307c2a332f7646dcfddb546a7970f62f08
jeberha <2620-jeberha@users.noreply.gitlab.gwdg.de>
2020-10-03 22:50:41 +0200

    Remove profile picture only if not default

--- src/safm_api/views.py
+++ src/safm_api/views.py
@@ -362,8 +362,9 @@ class UserProfileView(generics.GenericAPIView):
     
                     # Removes the old profile picture if a new one is uploaded
                     if request.data.get('profile_picture'):
-                        path_old_picture = os.path.join(settings.MEDIA_ROOT, profile.profile_picture.name)
-                        os.remove(path_old_picture)
+                        if not profile.has_default_picture:
+                            path_old_picture = os.path.join(settings.MEDIA_ROOT, profile.profile_picture.name)
+                            os.remove(path_old_picture)
 
                     profile = serializer.save()
 
