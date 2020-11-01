bbde3734792af909eb3250c02c768bcc7c8f85f0
g-wizzy <pierre.burki@protonmail.com>
2020-11-01 13:26:26 +0100

    Fix line length

--- src/safm_api/models/sample.py
+++ src/safm_api/models/sample.py
@@ -42,15 +42,44 @@ class Sample(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=50)
     file = models.FileField(max_length=255, upload_to=sample_path)
-    duration = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null=True) # duration in [s]
-    tempo = models.PositiveIntegerField(blank=True, null=True) # tempo is > 0
-    key = models.CharField(max_length=1, choices=Key.choices, blank=True, default=Key.NONE)
-    mode = models.CharField(max_length=3, choices=Mode.choices, blank=True, default=Mode.NONE)
-    description = models.TextField(blank=True, default='No description provided.')
-    datetime_upload = models.DateTimeField(auto_now_add=True) # auto now at creation
-    number_downloads = models.PositiveIntegerField(default=0) # nb dl > 0
-    tags = models.ManyToManyField(Tag) # a sample can have multiple tags
-    forks = models.ManyToManyField('self', related_name='forks_to', symmetrical=False)
+    # duration in [s]
+    duration = models.DecimalField(
+        max_digits=4,
+        decimal_places=1,
+        blank=True,
+        null=True
+    )
+    # tempo is > 0
+    tempo = models.PositiveIntegerField(
+        blank=True,
+        null=True
+    )
+    key = models.CharField(
+        max_length=1,
+        choices=Key.choices,
+        blank=True,
+        default=Key.NONE
+    )
+    mode = models.CharField(
+        max_length=3,
+        choices=Mode.choices,
+        blank=True,
+        default=Mode.NONE
+    )
+    description = models.TextField(
+        blank=True,
+        default='No description provided.'
+    )
+    datetime_upload = models.DateTimeField(
+        auto_now_add=True  # auto now at creation
+    )
+    number_downloads = models.PositiveIntegerField(default=0)  # nb dl > 0
+    tags = models.ManyToManyField(Tag)  # a sample can have multiple tags
+    forks = models.ManyToManyField(
+        'self',
+        related_name='forks_to',
+        symmetrical=False
+    )
 
     def deduce_properties(self):
         '''
